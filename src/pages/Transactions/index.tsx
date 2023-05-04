1c279eb4d90deb85242af1ef952f66756ac615ac
Avily Silva <avily.sva@gmail.com>
2023-05-04 16:29:19 -0300

    feat: transactions now is using server

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,40 +1,47 @@
+import { useEffect, useState } from 'react';
+
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 import { SearchForm } from './components/SearchForm';
 
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles';
+import { Transaction } from './interface';
 
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  async function loadTransactions() {
+    fetch('http://localhost:3004/transactions')
+      .then(response => response.json())
+      .then(data => {
+        setTransactions(data);
+      });
+  }
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
   return (
     <div>
       <Header />
       <Summary />
 
-
       <TransactionsContainer>
         <SearchForm />
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width='50%'>Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant='income'>
-                  R$ 17.000,00
-                </PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width='50%'>Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant='outcome'>
-                  - R$ 17.000,00
-                </PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
+            {transactions?.map(transaction => (
+              <tr key={transaction.id}>
+                <td width='50%'>{transaction.description}</td>
+                <td>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.price}
+                  </PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+            ))}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
--- src/pages/Transactions/interface.ts
+++ src/pages/Transactions/interface.ts
@@ -1,3 +1,12 @@
 export interface PriceHighlightProps {
   variant?: 'income' | 'outcome';
 }
+
+export interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  category: string;
+  price: number;
+  createdAt: string;
+}
