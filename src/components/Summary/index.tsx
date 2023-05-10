ccb4a5fb8706e93ab71411c5f41b19231ee349f2
Avily Silva <avily.sva@gmail.com>
2023-05-10 16:37:01 -0300

    feat: summary skeleton

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,13 @@
 import { useSummary } from '../../hooks/useSummary';
+import { useTransactions } from '../../hooks/useTransactions';
 import { priceFormatter } from '../../utils/formatter';
+import { Skeleton } from '../Skeleton';
 import { IncomeIcon, OutcomeIcon, SummaryCard, SummaryContainer, TotalIcon } from './styles';
 
 export function Summary() {
   const { income, outcome, total } = useSummary();
+
+  const { isLoading } = useTransactions();
 
   return (
     <SummaryContainer>
@@ -12,21 +16,33 @@ export function Summary() {
           <span>Entrada</span>
           <IncomeIcon />
         </header>
-        <strong>{priceFormatter.format(income)}</strong>
+        {isLoading ? (
+          <Skeleton style={{ height: '38px', marginTop: '1rem' }} />
+        ) : (
+          <strong>{priceFormatter.format(income)}</strong>
+        )}
       </SummaryCard>
       <SummaryCard>
         <header>
           <span>Saídas</span>
           <OutcomeIcon />
         </header>
-        <strong>{priceFormatter.format(outcome)}</strong>
+        {isLoading ? (
+          <Skeleton style={{ height: '38px', marginTop: '1rem' }} />
+        ) : (
+          <strong>{priceFormatter.format(outcome)}</strong>
+        )}
       </SummaryCard>
       <SummaryCard variant='green'>
         <header>
           <span>Total</span>
           <TotalIcon />
         </header>
-        <strong>{priceFormatter.format(total)}</strong>
+        {isLoading ? (
+          <Skeleton style={{ height: '38px', marginTop: '1rem' }} />
+        ) : (
+          <strong>{priceFormatter.format(total)}</strong>
+        )}
       </SummaryCard>
     </SummaryContainer>
   );
